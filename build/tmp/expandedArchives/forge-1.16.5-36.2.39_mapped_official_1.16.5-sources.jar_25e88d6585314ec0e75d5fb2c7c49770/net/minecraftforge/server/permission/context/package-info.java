836cbabeeef34480e5d6a6913f20408855373ab3
BoonelDanForever <boonerowley@gmail.com>
2023-05-06 16:26:33 -0600

    Remove ignored files

--- build/tmp/expandedArchives/forge-1.16.5-36.2.39_mapped_official_1.16.5-sources.jar_25e88d6585314ec0e75d5fb2c7c49770/net/minecraftforge/server/permission/context/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) Forge Development LLC and contributors
- * SPDX-License-Identifier: LGPL-2.1-only
- */
-
-@javax.annotation.ParametersAreNonnullByDefault
-@mcp.MethodsReturnNonnullByDefault
-package net.minecraftforge.server.permission.context;
